4686d8904fd2cca771b24663c37cde151e926ab2
tif-calin <culitif@tuta.io>
2021-04-26 13:58:08 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React from 'react';
 import { Component } from 'react';
 import logo from './react-logo.svg';
 import './App.css';
-import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Header/>
   
         My React App...
+        is lit
         
         <img src={logo} className="temp-images" alt="react logo" />
         <img src="acl-logo.png" className="temp-images" alt="acl logo" />
+
+        <Footer/>
       
       </div>
     );
   }
-
 }
 
 export default App;
