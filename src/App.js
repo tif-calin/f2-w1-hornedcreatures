120692bae5973848123405ad6d314bbf5ceaf892
tif-calin <76538249+tif-calin@users.noreply.github.com>
2021-04-26 15:55:03 -0700

    Revert "Revert "Dev""

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { Component } from 'react';
-import logo from './react-logo.svg';
-import './App.css';
 import React from 'react';
+import { Component } from 'react';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import CreatureList from './CreatureList';
+import creatures from './creatures.js';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Header/>
   
-        My React App...
-        
-        <img src={logo} className="temp-images" alt="react logo" />
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
+        <main>
+          <CreatureList creatures = {creatures}></CreatureList>
+        </main>
+
+        <Footer/>
       
       </div>
     );
   }
-
 }
 
 export default App;
