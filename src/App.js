fe912cc8d6a1c97154c78c2473881897e4185b01
tif-calin <culitif@tuta.io>
2021-04-27 16:39:34 -0700

    implement search on App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import Header from './Header';
 import BookSearch from './BookSearch';
 import ItemList from './ItemList';
 import Footer from './Footer';
-import books from './data.js';
+import bookData from './data.js';
 
 class App extends Component {
+  state = {
+    books: bookData
+  }
+
+  handleSearch(query, sort = null, field = 'title') {
+    const nameRegex = new RegExp(query, 'i');
+
+    const searched = bookData.filter(book => !query || book[field].match(nameRegex));
+    const sorted = searched.sort((a, b) => {
+      return 0;
+    });
+
+    this.setState({ books: sorted });
+  }
+
+  handleSort() {
+
+  }
 
   render() {
     return (
       <div className="App">
         <Header/>
 
-        <BookSearch/>
+        <BookSearch onSearch={this.handleSearch}/>
+
         <main>
-          <ItemList books = {books}></ItemList>
+          <ItemList books = {bookData}></ItemList>
         </main>
 
         <Footer/>
